aa174e67b75e7cf6893251b9f702a89c7c72aedd
SalvadorBValladolid <salvadorbv@ciencias.unam.mx>
2020-11-19 14:11:18 -0600

    Modifico script para traducir

--- Procesamiento_data/Translate_data.py
+++ Procesamiento_data/Translate_data.py
@@ -2,15 +2,22 @@
 import pandas as pd
 import csv
 from googletrans import Translator
-Data_entrenamiento=pd.read_csv("labeledTrainData.tsv",sep="\t")
-translator=Translator()
+import re
+Data_entrenamiento=pd.read_csv("/home/ec2-user/git/Bag-of-Words-Meets-Bags-of-Popcorn/Input/labeledTrainData.tsv",sep="\t")
+### Vamos a filtrar, para seleccionar solo una parte del dataset:
+renglones=input("Seleccina los renglones a traducir:")
+n=int(re.split("-",renglones)[0])
+u=int(re.split("-",renglones)[1])
+## Filtramos el dataset
+Data_entrenamiento=Data_entrenamiento.iloc[n:u,]
 
 # Comenzamos a tarducir a español
 reviews=list()
 for i in Data_entrenamiento["review"].values:
+    translator=Translator()
     reviews.append(translator.translate(i,dest="es").text)
     
 Data_entrenamiento["review_español"]=reviews
 # Lo guardamos como csv
-Data_entrenamiento.to_csv("Data entrenamiento.csv",sep=",",quotechar='"',index=False,
+Data_entrenamiento.to_csv("Data entrenamiento-"+renglones+".csv",sep=",",quotechar='"',index=False,
             quoting=csv.QUOTE_ALL)
